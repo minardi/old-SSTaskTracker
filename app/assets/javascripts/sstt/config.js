7ff0758862eef1f2fef7ae35d8db5b2c4b218b85
tTrilenko <trilenkotanya1991@gmail.com>
2013-08-29 02:13:12 +0300

    Update config.js

--- app/assets/javascripts/sstt/config.js
+++ app/assets/javascripts/sstt/config.js
@@ -1,5 +1,5 @@
 //= require ./run
 //= require ./projects/config
-//= require ./projects_info/config
+//= require ./project_info/config
 //= require ./stories/config
 //= require ./scrum-board/config
